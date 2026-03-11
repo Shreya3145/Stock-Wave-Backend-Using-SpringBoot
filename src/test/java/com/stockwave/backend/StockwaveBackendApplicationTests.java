cd5e9501742e27d3b000668d9ed01045015d0c3d
Shreya Ravichandiran <sudhashreya07@gmail.com>
2026-03-11 11:50:02 +0530

    Initial Commit of SpringBoot

---
+++ src/test/java/com/stockwave/backend/StockwaveBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stockwave.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockwaveBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
